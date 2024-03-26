5d0c8c46a57a8702b63995bfb20dda7ed700016f
Marco Castel <marcocastel123@gmail.com>
2024-03-25 21:11:53 -0500

    Sample Commit 3/25/24

---
+++ Assets/Scripts/Bottle_rtotation.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bottle_rtotation : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject bottle_1;
+
+    [SerializeField]
+    GameObject bottle_2;
+
+    [SerializeField]
+    GameObject bottle_3;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
