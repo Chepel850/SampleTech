6340910c43fac0c57031c5e2ef5cc64c40f110cf
Marco Castel <marcocastel123@gmail.com>
2024-03-29 18:06:53 -0500

    Sample Commit 3/29/24

--- Assets/Scripts/Bottle_rtotation.cs
+++ Assets/Scripts/Bottle_rtotation.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Bottle_rtotation : MonoBehaviour
 {
 
+
     [SerializeField]
     GameObject bottle_1;
 
@@ -13,6 +14,10 @@ public class Bottle_rtotation : MonoBehaviour
 
     [SerializeField]
     GameObject bottle_3;
+
+    public float speed_R = 6f;
+
+    public float Rotate_Angle = -45f;
 
 
     // Start is called before the first frame update
@@ -24,6 +29,32 @@ public class Bottle_rtotation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float tilt_Z = Input.GetAxis("Horizontal") * Rotate_Angle;
+        float tilt_x = Input.GetAxis("Vertical") * Rotate_Angle;
+
+        Quaternion B_Object = Quaternion.Euler(tilt_x, tilt_Z , -90);
+
+        bottle_1.transform.rotation = Quaternion.Slerp(transform.rotation, B_Object, Time.deltaTime * speed_R);
+
+        First_Bottle_Rotation();
+        Second_Bottle_Rotation();
+        Third_Bottle_Rotation();
     }
+
+    public void First_Bottle_Rotation()
+    {
+
+        bottle_1.transform.RotateAround(bottle_1.transform.position,  Vector3.up, Rotate_Angle);
+    }
+
+    public void Second_Bottle_Rotation()
+    {
+        bottle_2.transform.Rotate(0, speed_R ,0);
+    }
+
+    public void Third_Bottle_Rotation()
+    {
+        bottle_3.transform.Rotate(0, speed_R ,0);
+    }
+  
 }
